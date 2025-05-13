91633ce6e5b9714176bccf3755fc83be6f24b262
Téo <teo@haysberg.io>
2025-05-13 11:47:07 +0200

    Updated SQLite to run in memory and updated sources

--- utils/db.py
+++ utils/db.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, create_engine
 
 
-engine = create_engine("sqlite:///data/data.db", echo=False)
+engine = create_engine("sqlite:///:memory:", echo=False)
 SQLModel.metadata.create_all(engine)
