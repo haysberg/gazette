ec11cc117d90a06af4abea62de1a6700e6ca0460
Téo <teo@haysberg.io>
2025-05-14 21:28:17 +0200

    ruff format

--- utils/utils.py
+++ utils/utils.py
@@ -103,7 +103,7 @@ async def update_served_files() -> None:
                     "posts_24_48h": posts_24_48h,
                     "posts_later": posts_later,
                     "feeds": feeds,
-                    "render_time": datetime.now().strftime('%Hh%M').capitalize(),
+                    "render_time": datetime.now().strftime("%Hh%M").capitalize(),
                 },
             ).body.decode("utf-8")
             logger.info("HTML page rendered successfully !")
