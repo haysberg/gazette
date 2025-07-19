09fd4dcf2a32ac1856abc86a61a0a65651d1839b
Téo <teo@haysberg.io>
2025-07-19 21:52:09 +0200

    Optimized during build now

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
-# Stage 0
-# Installs dependencies and builds the application
-# Artifacts will be copied to the final image
+## BUILD STEP
+FROM ghcr.io/astral-sh/uv:python3.13-bookworm AS build
+WORKDIR /build
+RUN apt update -y && apt install imagemagick libpng-dev libjpeg-dev -y
+COPY config.toml build.py uv.lock pyproject.toml .
+RUN uv sync --frozen --no-cache --no-editable --compile-bytecode
+COPY static ./static/
+RUN uv run python build.py
+
+## PROD STEP
 FROM ghcr.io/astral-sh/uv:python3.13-alpine
 
 ENV UV_COMPILE_BYTECODE="1"
@@ -9,14 +16,15 @@ ENV TZ=Europe/Paris
 
 WORKDIR /app
 
-COPY config.toml app.py uv.lock uv.lock pyproject.toml .
+COPY config.toml app.py uv.lock pyproject.toml .
 COPY templates ./templates/
 COPY utils ./utils/
-COPY static ./static/
+COPY --from=build /build/static ./static/
 COPY --from=joseluisq/static-web-server:2-alpine /usr/local/bin/static-web-server /bin/static-web-server
 
+RUN apk update --no-cache
 RUN uv sync --frozen --no-cache --no-dev --no-editable --compile-bytecode
-RUN apk update && apk add cairo
+
 
 EXPOSE 8000
 
--- utils/models.py
+++ utils/models.py
@@ -1,16 +1,10 @@
 import asyncio
 from datetime import datetime
-from io import BytesIO
-import os
 from time import mktime
 
 import feedparser
-import cairosvg
 from utils.logs import logger
-import httpx
 from sqlmodel import Field, Relationship, SQLModel, Session
-from sqlmodel.ext.asyncio.session import AsyncSession
-from PIL import Image
 from utils import engine
 
 
@@ -21,37 +15,6 @@ class Feed(SQLModel, table=True):
     subtitle: str = Field(default="")
     image: str = Field(default="")
     posts: list["Post"] = Relationship(back_populates="feed")
-
-    async def download_image(self):
-        for i in range(3):
-            try:
-                image_path = os.path.join("static", "favicons", f"{self.domain}.webp")
-                os.makedirs(os.path.dirname(image_path), exist_ok=True)
-
-                async with httpx.AsyncClient(timeout=10, verify=False) as client:
-                    response = await client.get(self.image)
-                    try:
-                        response.raise_for_status()
-                    except httpx.HTTPStatusError as http_err:
-                        logger.error(
-                            f"HTTP error while downloading image for {self.domain}: {http_err}"
-                        )
-                        return
-
-                img_data = BytesIO(response.content)
-                if self.image.endswith(".svg"):
-                    png_data = BytesIO()
-                    cairosvg.svg2png(bytestring=img_data.getvalue(), write_to=png_data)
-                    img_data = png_data
-
-                Image.open(img_data).resize((32, 32), Image.LANCZOS).save(
-                    image_path, "WEBP"
-                )
-                logger.debug(f"Image for {self.domain} saved to {image_path}")
-            except Exception as e:
-                logger.error(f"Failed to save image for {self.domain}: {e}")
-                continue
-            break
 
     @classmethod
     async def init_feed(cls, feed_dict: dict):
@@ -77,7 +40,6 @@ class Feed(SQLModel, table=True):
             with Session(engine) as session:
                 session.merge(feed)
                 session.commit()
-            asyncio.create_task(feed.download_image())
             return feed
 
         except (AttributeError, KeyError) as e:
