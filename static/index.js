5b896565c09554d57b782c56dd35cd3d709e3bcb
haysberg <teo@haysberg.io>
2025-08-25 19:21:47 +0200

    Revert JS

--- static/index.js
+++ static/index.js
@@ -1,24 +1,30 @@
-if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone) {
-    console.log("Déjà installé.")
-} else {
-    if (typeof window.onbeforeinstallprompt == "object") {
-        console.log("This browser supports the beforeinstallprompt event.");
-        document.getElementById("install-button").style.display = "block";
-        let installPrompt = null;
-        const installButton = document.querySelector("#install-button");
-        window.addEventListener("beforeinstallprompt", e => {
-            e.preventDefault();
-            installPrompt = e;
-            installButton.addEventListener("click", async () => {
-                if (!installPrompt) return;
-                const r = await installPrompt.prompt();
-                console.log(`Install prompt was: ${r.outcome}`);
-                installPrompt = null;
-                installButton.setAttribute("hidden", "");
-            });
-            installButton.removeAttribute("hidden");
+const isPWA =
+    !!(
+        window.matchMedia?.('(display-mode: standalone)').matches ||
+        window.matchMedia?.('(display-mode: fullscreen)').matches ||
+        window.navigator.standalone
+    );
+
+if (
+    typeof window.onbeforeinstallprompt == "object" &&
+    !isPWA
+) {
+    console.log("This browser supports the beforeinstallprompt event.");
+    document.getElementById("install-button").style.display = "block";
+    let installPrompt = null;
+    const installButton = document.querySelector("#install-button");
+    window.addEventListener("beforeinstallprompt", e => {
+        e.preventDefault();
+        installPrompt = e;
+        installButton.addEventListener("click", async () => {
+            if (!installPrompt) return;
+            const r = await installPrompt.prompt();
+            console.log(`Install prompt was: ${r.outcome}`);
+            installPrompt = null;
+            installButton.setAttribute("hidden", "");
         });
-    } else {
-        console.log("This browser does not support the beforeinstallprompt event.");
-    }
+        installButton.removeAttribute("hidden");
+    });
+} else {
+    console.log("This browser does not support the beforeinstallprompt event.");
 }
